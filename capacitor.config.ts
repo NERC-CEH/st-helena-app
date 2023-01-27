a17a4c62769b0c97dc945244603b3d0b1e9fddda
vilius-stankaitis <vilius@stankaitis.com>
2023-01-27 14:25:29 +0200

    Update appId

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli'; // eslint-disable-line
 
 const config: CapacitorConfig = {
-  appId: 'uk.ac.ceh.irecord-st-helen',
+  appId: 'uk.ac.ceh.irecord.st.helena',
   appName: 'iRecord St Helena App',
   bundledWebRuntime: false,
   webDir: 'build',
